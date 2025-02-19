15b97d0dfc460e62f9426a8e24fc14b3a0c87db2
pkncsk <pkncsk@gmail.com>
2025-02-19 03:38:10 +0000

    udate version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ma_mapper',
-    version='0.8.3',
+    version='0.8.3.1',
     packages=find_packages(),
     author= 'Pakkanan',
     author_email= 'pc575@cam.ac.uk',
